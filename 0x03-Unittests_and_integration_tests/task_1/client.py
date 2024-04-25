4137bb983e7443764aa0561432e22a4c7411b5a8
Victor-kay <jvictorkingorikay94@gmail.com>
2024-04-25 18:16:28 +0000

    Parameterize a unit test

---
+++ 0x03-Unittests_and_integration_tests/task_1/client.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+This module provides a client class for a different service.
+"""
+
+from typing import List, Dict
+
+# ... (Same as task_0/client.py)
---
+++ 0x03-Unittests_and_integration_tests/task_1/utils.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+This module provides utility functions for manipulating data.
+"""
+
+from typing import Any, Dict, Union
+
+# ... (Same as task_0/utils.py)
